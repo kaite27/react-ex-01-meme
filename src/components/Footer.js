02af1f7c82fbebd284ff640b0cf54304985bfd3f
beigenut <beigenut@gmail.com>
2019-07-11 17:38:37 -0400

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return(
+    <React.Fragment>
+      <footer>
+        <p>Laborum ipsum laboris ut fugiat esse.</p>
+        <ul>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </footer>
+    </React.Fragment>
+  )
+}
+
+export default Footer
